6c53d9a8efbed7accc2cfc62cf386a16d27e1560
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-25 23:04:33 +0500

    Task009 completed.

---
+++ Lesson001/Task009.py
@@ -0,0 +1,6 @@
+import RefFunctions
+
+print("Задание 9. Программа, которая на вход принимает 5 чисел и находит максимальное из них.")
+print("Введите значения: ")
+print(
+    f"Максимально значение: {RefFunctions.Sort_Array(RefFunctions.Filling_One_dimensional_Array(5))}")
