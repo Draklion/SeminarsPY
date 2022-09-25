71d8a437417d69d126f5c277bbdb7f74bdd76822
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-25 19:31:57 +0500

    Task006 completed.

---
+++ Lesson001/RefFunctions.py
@@ -0,0 +1,11 @@
+def Filling_One_dimensional_Array(count):
+
+    Filled_Array = []
+    for i in range(count):
+        number = int(input())
+        Filled_Array.append(number)
+    return Filled_Array
+
+
+def Print_One_dimensional_Array(Source_Array):
+    return print(Source_Array)
