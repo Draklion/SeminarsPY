267d68be6c213ed27b477af6971c37f5ae16fdc1
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-26 00:23:16 +0500

    Test.

---
+++ Lesson001/Test.py
@@ -0,0 +1,5 @@
+from Ref.Ref import *
+
+print("Задание 9. Программа, которая на вход принимает 5 чисел и находит максимальное из них.")
+print("Введите значения: ")
+print(Sort_Array(Filling_One_dimensional_Array(5)))
