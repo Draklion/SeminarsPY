12ac26ee8cd068254742eb90889c71c23cd19990
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-02 17:28:16 +0500

    Lesson003_Task006 completed.

---
+++ Lesson003/Task006.py
@@ -0,0 +1,19 @@
+print("Задание 6. Программа, которая составляет список чисел Фибоначчи, в том числе для отрицательных индексов.")
+
+number = int(input("Введите число: "))
+if number <= 1:
+    print("")
+else:
+    firstNumberFibonacci = 0
+    secondNumberFibonacci = 1
+    Fibonacci_list = [0, 1]
+    Fibonacci_reverse_list = []
+    for i in range(2, number+1):
+        Fibonacci_list.append(firstNumberFibonacci+secondNumberFibonacci)
+        firstNumberFibonacci = secondNumberFibonacci
+        secondNumberFibonacci = Fibonacci_list[i]
+    for i in range(1, len(Fibonacci_list)):
+        Fibonacci_reverse_list.append(
+            Fibonacci_list[len(Fibonacci_list)-i]*(-1)**i)
+    Fibonacci_reverse_list.extend(Fibonacci_list)
+    print(Fibonacci_reverse_list)
