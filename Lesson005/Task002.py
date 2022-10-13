419adc0f7a57782e71eaa34010675af29759ebea
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-14 01:02:25 +0500

    Lesson005_Task002 changed.

--- Lesson005/Task002.py
+++ Lesson005/Task002.py
@@ -6,10 +6,10 @@ path_to_file_tcw = "text_code_words.txt"
 text_code_words = open(path_to_file_tcw, "w")
 for i in range(2):
     text_code_words.write(f"{Archiver(path_to_file_twt, i)}\n")
-sleep(2)
+print()
 print("Данные записаны в файл.")
 print()
-sleep(1)
 print("Данные разархивированы.")
+print()
 text_code_words.close()
 Unarchiver(path_to_file_tcw)
