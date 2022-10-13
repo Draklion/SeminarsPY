2f2663a99f9d64b02c8af77ceff2d0596cf8bb14
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-13 21:37:21 +0500

    Lesson005_Task002 changed.

---
+++ Lesson005/Task002.py
@@ -0,0 +1,18 @@
+from math import radians
+from operator import le
+
+
+print("Задание 2. Программа,  реализует модуль сжатия и восстановления данных.")
+first_string = open("text_words.txt").readlines()[:2]
+count = 1
+for i in range(len(first_string[0])):
+    current_string = str(first_string[0][0:len(first_string[0])-1])
+    print(current_string)
+    current_str = current_string[0]
+    for i in range(0, len(current_string)):
+        if current_string[i] == current_str:
+            count += 1
+        else:
+            current_str == current_string[i]
+            count = 1
+    print(count, current_str)
