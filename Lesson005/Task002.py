6a22e71abbc02596deb19d2dff52a85731bec738
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-14 00:59:26 +0500

    Lesson005_Task002 completed.

--- Lesson005/Task002.py
+++ Lesson005/Task002.py
@@ -1,3 +1,4 @@
+from time import sleep
 from RefFunctions import Archiver, Unarchiver
 print("Задание 2. Программа,  реализует модуль сжатия и восстановления данных.")
 path_to_file_twt = "text_words.txt"
@@ -5,5 +6,10 @@ path_to_file_tcw = "text_code_words.txt"
 text_code_words = open(path_to_file_tcw, "w")
 for i in range(2):
     text_code_words.write(f"{Archiver(path_to_file_twt, i)}\n")
+sleep(2)
+print("Данные записаны в файл.")
+print()
+sleep(1)
+print("Данные разархивированы.")
 text_code_words.close()
-print(Unarchiver(path_to_file_tcw))
+Unarchiver(path_to_file_tcw)
