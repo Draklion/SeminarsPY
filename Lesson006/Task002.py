99529106872182902a8517d5e87c298e5c845c1d
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-14 21:15:16 +0500

    Added changed.

---
+++ Lesson006/Task002.py
@@ -0,0 +1,6 @@
+from Lesson003.RefFunctions import Filling_One_Dimensional_Array as FODA
+import sys
+sys.path.append('Lessons PY/SeminarsPY/Lesson003/RefFunctions.py')
+print("Задание 2. Программа, которая находит числа, кратные 20 или 21 для чисел в пределах от 20 до N.")
+number = int(input("Введите число: "))
+FODA(number)
---
+++ init.py
@@ -0,0 +1,3 @@
+from Lesson003.RefFunctions import Filling_One_Dimensional_Array as FODA
+
+print(FODA(5))
