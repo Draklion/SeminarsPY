3d9d37b9d58d6267d963022bc362e91ca17807b8
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-14 20:44:34 +0500

    Lesson006_Task001 changed.

--- Lesson006/RefFunctions.py
+++ Lesson006/RefFunctions.py
@@ -1,6 +1,14 @@
 from random import randint
 
 
-def Сomparison(path_to_fail):
-    current_string = open(path_to_fail).readline()[1].split()
-    return current_string
+def Сomparison(path_to_file):
+    current_list = open(path_to_file).readlines()[
+        randint(0, 1)].rstrip().split(", ")
+    print(f"Исходный список: {current_list}")
+    result_list = [current_list[i+1]
+                   for i in range(0, len(current_list)-1, 2) if int(current_list[i]) < int(current_list[i+1])]
+    if len(current_list) % 2 != 0:
+        result_list.append(current_list[len(current_list)-1])
+    else:
+        result_list
+    return result_list
--- Lesson006/Task001.py
+++ Lesson006/Task001.py
@@ -1,5 +1,4 @@
 from RefFunctions import Сomparison
 print("Задание 1. Программа, которая выводит элементы исходного списка, значения которых больше предыдущего элемента.")
 
-
 print(Сomparison("Task001.txt"))
