6c89fc9f2b6b8f110b32d437a323cf3ea601b542
«Dmitry_Machnev» <draklion56@gmail.com>
2022-10-14 18:43:51 +0500

    Added files.

---
+++ Lesson006/RefFunctions.py
@@ -0,0 +1,6 @@
+from random import randint
+
+
+def Сomparison(path_to_fail):
+    current_string = open(path_to_fail).readline()[1].split()
+    return current_string
---
+++ Lesson006/Task001.py
@@ -0,0 +1,5 @@
+from RefFunctions import Сomparison
+print("Задание 1. Программа, которая выводит элементы исходного списка, значения которых больше предыдущего элемента.")
+
+
+print(Сomparison("Task001.txt"))
